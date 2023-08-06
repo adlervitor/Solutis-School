82eae90a25df80c5276c37e41df18729e598dfad
Adler Vitor Santiago B <125394284+adlervitor@users.noreply.github.com>
2023-08-06 16:01:38 -0300

    Add files via upload

---
+++ Exercícios Lista 0/Questão 07/TrocaValores.java
@@ -0,0 +1,32 @@
+//Questão 07
+
+import java.util.Scanner; // Importa o scanner
+
+public class TrocaValores {
+
+    public static void main(String[] args) {
+    		
+        Scanner scanner = new Scanner(System.in); // Inicializa o scanner
+        
+        // Lê os valores
+        System.out.print("Valor de A: ");
+        double A = scanner.nextDouble();
+        
+        System.out.print("Valor de B: ");
+        double B = scanner.nextDouble();
+        
+        // Troca dos valores
+        double temp = A;
+        A = B;
+        B = temp;
+        
+        // Valores trocados
+        System.out.println("Valores trocados:");
+        System.out.println("A = " + A);
+        System.out.println("B = " + B);
+        
+        scanner.close(); // Fecha o scanner
+        
+    }
+	
+}
