75c2d18989d811d78e07e4e03793f023fd865b10
Adler Vitor Santiago B <125394284+adlervitor@users.noreply.github.com>
2023-08-06 15:31:35 -0300

    Update TrocaValores.java

--- Exercícios Lista 0/TrocaValores.java
+++ Exercícios Lista 0/TrocaValores.java
@@ -7,25 +7,26 @@ public class TrocaValores {
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in); // Inicializa o scanner
+			
+			// Lê os valores
+	    System.out.print("Valor de A: ");
+	    double A = scanner.nextDouble();
+	
+		System.out.print("Valor de B: ");
+		double B = scanner.nextDouble();
+	
+		// Troca dos valores
+		double temp = A;
+		A = B;
+		B = temp;
+	
+		// Valores trocados
+		System.out.println("Valores trocados:");
+		System.out.println("A = " + A);
+		System.out.println("B = " + B);
+	
+		scanner.close(); // Fecha o scanner
 		
-		// Lê os valores
-        System.out.print("Valor de A: ");
-        double A = scanner.nextDouble();
-
-        System.out.print("Valor de B: ");
-        double B = scanner.nextDouble();
-
-        // Troca dos valores
-        double temp = A;
-        A = B;
-        B = temp;
-
-        // Valores trocados
-        System.out.println("Valores trocados:");
-        System.out.println("A = " + A);
-        System.out.println("B = " + B);
-
-        scanner.close(); // Fecha o scanner
-    }
+	}
 	
 }
