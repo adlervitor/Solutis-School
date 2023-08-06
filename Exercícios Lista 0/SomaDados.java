9c2ad4e1cab03338292815a28e0bcaf66a34dbe0
Adler Vitor Santiago B <125394284+adlervitor@users.noreply.github.com>
2023-08-06 15:30:23 -0300

    Update SomaDados.java

--- Exercícios Lista 0/SomaDados.java
+++ Exercícios Lista 0/SomaDados.java
@@ -2,11 +2,11 @@
 
 public class SomaDados {
 	
-    public static void main(String[] args) {
+	public static void main(String[] args) {
         for (int dado1 = 1; dado1 <= 6; dado1++) {
             for (int dado2 = 1; dado2 <= 6; dado2++) {
                 if (dado1 + dado2 == 7) {
-                    System.out.println("Possibilidades de soma: " + dado1 + ", " + dado2);
+					System.out.println("Possibilidades de soma: " + dado1 + ", " + dado2);
                 }
             }
         }
