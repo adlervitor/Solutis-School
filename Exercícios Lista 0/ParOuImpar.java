a686cfdd366717db076d83372aec6c60732c6c15
Adler Vitor Santiago B <125394284+adlervitor@users.noreply.github.com>
2023-08-06 15:34:59 -0300

    Update ParOuImpar.java

--- Exercícios Lista 0/ParOuImpar.java
+++ Exercícios Lista 0/ParOuImpar.java
@@ -6,19 +6,19 @@ public class ParOuImpar {
 	
 	public static void main(String[] args) {
 		
-	  Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 	  
-      System.out.print("Digite um numero: ");
-      double num = scanner.nextDouble();
-      
-      //Verifica se é par
-      if(num % 2 == 0) {
-    	  System.out.println("O numero digitado eh par");
-      } else {
-    	  System.out.println("O numero digitado eh impar");
-      }
-      
-      scanner.close();
+	    System.out.print("Digite um numero: ");
+	    double num = scanner.nextDouble();
+	      
+	    //Verifica se é par
+	    if(num % 2 == 0) {
+	    	System.out.println("O numero digitado eh par");
+	    } else {
+	    	System.out.println("O numero digitado eh impar");
+	    }
+		
+	    scanner.close();
       
   }
 	
